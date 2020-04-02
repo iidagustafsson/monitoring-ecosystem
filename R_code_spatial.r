452e79004e5eb31f981ba0766af43295de7756be
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-04-02 09:28:01 +0200

    Create R_code_spatial.r

---
+++ R_code_spatial.r
@@ -0,0 +1,118 @@
+### R code for spatial view of points
+
+what to do today - show the point we have seen last time on a map
+
+recall the library that we used last time
+library(sp)
+
+recall the dataset meuse
+data(meuse)
+
+Look at the data - looking at some data. the first 6 rows of data
+head(meuse)
+
+we can see that the data containt some spatial data. points in space. they have coordinaates. x and y
+
+# coordinates
+tell the dataset this is a spatial dataset. tell the dataset that the coordinates are x and y
+coordinates(meuse)=~x+y
+
+now we are going to think spatial. make functions that use spatial infromation
+the dataphrame from a spatial data to plot
+plot(meuse) 
+
+one of the functions that are contaning in the sp packages. explain which dataset you are using. 
+and then make use of one of the virables example zinc
+spplot(meuse,"zinc")
+
+for using the plot. you will se the spots in space.
+by using the spplot, 
+
+see how the other elements/virable are related to the spatial amount
+# excercise: plot the sptaial amount of copper
+spplot(meuse,"copper")
+
+change the titel of the graph. using main=
+spplot(meuse,"copper",main="copper concentration")
+
+we can see the cooleration. 
+where there is a higher amount of zinc, there is also an higher concentration of copper.
+this means that the river is brining this material. 
+
+an othjer function. tyo see the same information. but changing the size of pints, instead of color
+it is called bubble. put in the dataset, the virable we want to show in bubble
+bubble(meuse,"zinc")
+it is the same information as the previous map. but making use of the amount of the element. 
+larger bubble means a higher concentration
+
+# exercise: bubble copper in red
+bubble(meuse,"copper",col="red",main="Copper Concentration")
+
+# Importing new data. Making use of the file covid.agg
+download covid.agg from iol. 
+create a folder called Lab on desktop C
+now we should transfer this dataset into R
+
+# setting the working directory : Lab
+For windows users. setwd. Than recall the desktop where we stored the folder
+setwd("C:/Lab/")
+
+Give name of the dataset we want to use - covid
+Link the name to the functions. Linking the function to an object <-
+Dataset will be called covid. What is the name of the table we are going to read?
+We are going to read the table in covid read.table
+Previously we have told R to make use of the file in folder Lab
+The first row in the dataset is just a heading. There is a heading. 
+Need to tell R this, by putting head=T
+covid<-read.table("covid_agg.csv",head=T)
+
+attach the dataset
+attach(covid)
+
+make a plot of country and cases. X-axis = country, and Y-axis = cases
+plot(country,cases)
+
+
+number of cases by each country. the highest amount of cases were in china. 
+making the name of the countries vertical, in order to see all the countries.
+labes = las. 4 differnt codes. 
+
+plot(country,cases,las=0)
+nothing happened in R. making parrallel labels
+
+try a new label. las=1
+plot(country,cases,las=1)
+=>all the labels of the horizontal labels
+
+Try a new label. las=2
+plot(country,cases,las=2)
+=>this changes the name of the countries horisontal
+
+Try a new label. las=3
+plot(country,cases,las=3)
+vertical labels
+
+
+plot(country,cases,las=3,cex.axis=0.5)
+making x-axis half of the levels as before
+
+lets plot the dataset spatially
+make us of the ggplot2 - make beautiful graphs. 
+it is a library that can be installed
+the main patterns that we can achieve by ggplot. 
+to make a ggplot. need 3 main components
+1. the dataset we are going to make use of (in our case - covid)
+2. aesthetic mapping (the virables we want to put in the graph) (in our case is the country + cases)
+3. the symbol we want to show this data (e.g. points)
+
+install the packaged called ggplot2
+install.packages("ggplot2")
+
+explian to R that we want to make use of the package ggplot2
+library(ggplot2)
+
+save the data. go to File => Save workspace => in the mapfolder Lab in C. I called it .RData
+.RData under the menu File
+
+
+
