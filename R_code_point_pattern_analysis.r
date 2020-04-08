40cf5361a67d074e0dbd3588d476f223e217fc85
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-04-08 21:22:50 +0200

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -69,7 +69,130 @@ points(covids)
 
 save the workspace - density.RDate
 
+------------------- 2020-04-08---------------
+re-open density
+Load previous workspace. Tell R from where to load from
+setwd("C:/Lab/")
+load(".RData")
 
+To see which datasets we used last time
+ls()
+= covid, covids, d, meuse, mpg
+  
+Recall library to make spatial map
+library(spatstat) 
+  
+Plot a density map, The density map we plotted last time. saved as d
+plot(d)
+points(covids)
 
+Install the packages rgdal. Since it is outside of R "
+install.packages("rgdal")
+library(spatstat)
+library(rgdal)
+plot(d)
+points(covids)
 
+Download file called coastlines from iol
+Extract the zipped file into the created Lab-folder
+lets input vector lines to the map
+coastlines<-readOGR("ne_10m_coastline.shp")
 
+- creating worldmap lines
+plot(d)
+points(covids)
+plot(coastlines,add=T)
+
+lets see the ecological point. not dealing with the number of cases
+we are seeing the density of how many case in countries
+use a command that will be usefull by monitoring ecosystems
+changing the aspects. changing the colors of the map
+
+We are going to make an pallet. give it a namne cl
+We want to adress it: <-
+
+State to the software. The color range we want to switch inbetween
+Instead of how it is now from blue to yellow color
+
+To indicate a range. Build a cluster: (c)
+The colors we want. First color for the smaller values, last color for largest values. 
+ "yellow","orange","red"
+Our map will range from yellow to red. Red color for highest density of cases.
+
+To wider the range of different colors, add number in end
+(100)
+
+Plot the density, by the colorpalette we make
+cl<-colorRampPalette(c("yellow","orange","red")) (100)
+plot(d,col=cl, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+
+####Exercise: new color ramp palette
+clr<-colorRampPalette(c("blue","green","yellow"))(100)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+
+clr<-colorRampPalette(c("blue","green","yellow","orange"))(100)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+
+clr<-colorRampPalette(c("blue","green","yellow","orange","red"))(100)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+  
+Do a new Ramp Palette
+Change the number of colors. Into a small number of colors
+clr<-colorRampPalette(c("blue","green","yellow","orange","red"))(5)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+
+clr<-colorRampPalette(c("blue","green","yellow","orange","red"))(1000)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+  
+By doing so, it will be a less beautiful map. 
+Changing from one color into another color very abbrubt.
+The number is representing the final numer of color we are using, in the last case 5.
+By chaning into a higher number, it will make it into a smooth change. 
+    
+  
+How to export such a map?
+We are going to make an pdf of the map.
+Give it the namn covid_density
+pdf("covid_density.pdf")
+  
+Copy all of the functions used for plotting
+clr<-colorRampPalette(c("blue","green","yellow","orange","red"))(1000)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+  
+close the devise
+dev.off()
+  
+pdf("covid_density.pdf")
+clr<-colorRampPalette(c("blue","green","yellow","orange","red"))(1000)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+dev.off()
+  
+The map will now be saved into the lab folder.
+And will be called covid_density
+  
+You can also create png files
+png("covid_density.png")
+clr<-colorRampPalette(c("blue","green","yellow","orange","red"))(1000)
+plot(d,col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines,add=T)
+dev.off()
+
+pdf-files have a higher quality when you zoome in. 
+We started with countries, cases, latitude, longitude and ended with a nice map!
