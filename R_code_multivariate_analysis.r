6a680995724ee40d6f8cf8250495d1e9b6490357
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-04-08 18:06:26 +0200

    Create R_code_multivariate_analysis.r

---
+++ R_code_multivariate_analysis.r
@@ -0,0 +1,46 @@
+############ Multivariate Analysis
+How the different communites are changing in time
+And how we can monitore the changes
+
+Start with in-situ data collection
+Going out in the field to get information about the species that live in the field
+
+We are dealing with communities. Also soil, temperature, pH etc
+
+Butterflies first plot - 13 , second plot - 1 and third plot - 2
+Orchys: first plot - 15, second plot - 2 and third plot - 3
+Larix: first plot - 3, second plot - 10 and third plot - 8
+
+See how the 3 species are related to each other
+One manner to see this data. 
+Do a matrix/table of each plot. And see where the species have been find + how many individuals
+As we can see the the butterflies and orchys are more related, rather than butterflies and tree
+
+Take number of the individuals as the value of the 
+x-axis = plot 1 
+y-axis = plot 2 
+z-axis = plot 3 
+
+Species that are more related are closer to each other
+Species that are less related are farer away
+
+The functional relationship, in our case, is the relationhip between butterflies and orchys
+Study ecosystem functions and relationships. Which have been studied in a certain area
+
+What if we use a 4 plots?
+How to show 4 plots in such a graph?
+It is impossible. We can only see 3 dimensions. 
+The forth one is time
+
+How to reduce a 4th dimension model into a 3th dimension object?
+Several plots. That should be reduced into 2 dimension
+Multi=Several
+Varib=variables
+
+The variation is seen 
+Explain the variation. To show the variation. 
+To see the variation, you need the variations in plot 1 and plot 2
+Plot 1 explained 50% of variation. Same for plot 2
+Component 1 explain 90% of the variation, and component 2 explain 10%
+
+
