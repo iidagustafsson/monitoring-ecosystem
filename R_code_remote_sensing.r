ca4cad26ce4ac8041c3f89797f9b81f85912d942
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-04-17 17:55:55 +0200

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,8 @@
+R code for remote sensing data analysis
+
+install.packages("raster")
+install.packages("RStoolbox")
+install.packages(c("rasteer","RStoolnox"))
+
+Unzip the downloaded file into the Lab folder. Make sure they are unpacked.
+
