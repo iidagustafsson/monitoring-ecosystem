75d4db1c22725163f9d1307aa2be13082d783a8c
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-05-01 12:27:44 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -2,7 +2,7 @@ R code for remote sensing data analysis
 
 install.packages("raster")
 install.packages("RStoolbox")
-or use: install.packages(c("rasteer","RStoolnox"))
+or use: install.packages(c("rasteer","RStoolbox"))
 ----------------------------------------------------
 
 2020-02-22
@@ -23,7 +23,7 @@ path: 224 row: 63
 The files are from 2011 and 1988. In these years the images have been taken
 
 Import the images from the lab folder
-The funkction to import images into R is called : brick
+The function  to import images into R is called : brick
 Importing all of the packs of bands
 
 Go to the lab folder - .grd extension are the images (the extension cannot be seen in windows)
@@ -41,7 +41,7 @@ plot(p224r63_2011)
 Vegetation is reflected by the Near Infrared (NIR) 
 High reflection in Green light, very little reflection in RED and BLUE due to photosynthesis
 
-Change the color ramp palletes of the plots
+Change the color ramp pallets  of the plots
 The color ramp is now from white to green colors
 We are going to change it into a grey scale ramp
 
@@ -53,22 +53,22 @@ Bands of landset
 First band B1: Blue
 Second band B2: Green
 Third band B3: Red
-Fourht band B4: NIR
-Fifht band B5: Medium Infra Red
+Fourth  band B4: NIR
+Fifth band B5: Medium Infra Red
 Sixth band B6: Thermal Infra Red
-Seveneht band B7: Medium Infra Red
+Seventh  band B7: Medium Infra Red
 
 Lets make a different plot of the image
 Plot the single band. Just one of them
 Each of them can be colored with a single color ramp palette
 
 Make us of the function: par
-Make a multifram row: mfrow
-Explain the amount of rows and colums you want: 2,2
-To put the number of colums and graphs into the graph. Make a series of charachter (c)
+Make a multi frame row: mfrow
+Explain the number of rows and colums you want: 2,2
+To put the number of colums and graphs into the graph. Make a series of character (c)
 par(mfrow=c(2,2))
 
-To see all the colors availiable in R
+To see all the colors available in R
 color()
 
 We are going to plot the B1 band. 
@@ -78,11 +78,11 @@ Use the $ symbol to link all the bands to the images
 The name of the band B1 = B1_sre
 plot(p224r63_2011$B1_sre,col=clb)
 
-### Exercies - do the same for green band. Which is band B2
+### Exercise - do the same for green band. Which is band B2
 clg<-colorRampPalette(c("dark green", "green","light green"))(100)
 plot(p224r63_2011$B2_sre,col=clg)
 
-### Exercies - do the same for red band. Which is band B3
+### Exercise - do the same for red band. Which is band B3
 clr<-colorRampPalette(c("dark red", "red","pink"))(100)
 plot(p224r63_2011$B3_sre,col=clr)
 
@@ -111,7 +111,7 @@ Show a plot - RGB
 R-red =3, G-green =2, B-blue=1
 The function is called: plotRGB
 
-Stretch to see the colors, by making a liniar stretching. 
+Stretch to see the colors, by making a linear stretching. 
 The function is called: stretch="Lin"
 plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin")
 
@@ -149,7 +149,7 @@ I called it remote_sensing
 Set my working directory
 setwd("C:/lab/")
 
-Load previous workingspace
+Load previous working space
 Load the file called remote_sensing into R
 
 Now load to see all the dataset we used last time
@@ -173,10 +173,10 @@ Bands of landset (same as for 2011)
 First band B1: blue
 Second band B2: green
 Third band B3: Red
-Fourht band B4: NIR
-Fifht band B5: Medium Infra Red
+Fourth band B4: NIR
+Fifth band B5: Medium Infra Red
 Sixth band B6: Thermal Infra Red
-Seveneht band B7: Medium Infra Red
+Seventh band B7: Medium Infra Red
 
 ### Exercise: plot in visible RGB 3 2 1 both the images. So plot in 2 rows and 1 column
 par(mfrow=c(2,1))
@@ -189,8 +189,8 @@ par(mfrow=c(2,1))
 plotRGB(p224r63_1988,r=4,g=3,b=2,stretch="Lin")
 plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
 
-By using the NIR, it is the most powerful band in order to monitore ecosystems 
-Here you can see how much humans have opened up the forest. Humas have fragmenteted the forest
+By using the NIR, it is the most powerful band in order to monitor ecosystems 
+Here you can see how much humans have opened up the forest. Humans have underneath the forest
 Which have created problems for the people living in the forest 
 But also creating problems for animals and plants
 Creating new niches for alien species to adapt to. Will compete with the endemic species.
@@ -198,8 +198,8 @@ Creating new niches for alien species to adapt to. Will compete with the endemic
 Use another stretch
 Lets make us of a histogram stretch: "hist"
 This function enhances the 'noise'. 
-In this case it is mainly related to humidity. The evapotranspiration process of the forest will be visable. 
-Put the both images in the same plot, but underneeth one and another. Still using RGB 4 3 2 in both images
+In this case it is mainly related to humidity. The evapotranspiration process of the forest will be visible 
+Put the both images in the same plot, but underneath one and another. Still using RGB 4 3 2 in both images
 par(mfrow=c(2,1))
 plotRGB(p224r63_1988,r=4,g=3,b=2,stretch="hist")
 plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="hist")
@@ -219,16 +219,16 @@ Bands of landset
 First band B1: blue
 Second band B2: green
 Third band B3: Red
-Fourht band B4: NIR
-Fifht band B5: Medium Infra Red
+Fourth band B4: NIR
+Fifth band B5: Medium Infra Red
 Sixth band B6: Thermal Infra Red
-Seveneht band B7: Medium Infra Red
+Seventh band B7: Medium Infra Red
 
 Lets give it the name to the index : dvi2011
 To link the band to the image, make use of $.  
 dvi2011<-p224r63_2011$B4_sre-p224r63_2011$B3_sre
 
-Lets use a differnt color ramp palette
+Lets use a different color ramp palette
 cl<-colorRampPalette(c("darkorchid3", "light blue","lightpink4"))(100)
 plot(dvi2011,col=cl)
 
@@ -248,18 +248,18 @@ The differences: dvi2011 - dvi1988
 diff<-dvi2011 - dvi1988
 plot(diff)
 
-Green parts = where the DVI changes alot
-Brown parts = negativly affected. By random events. 
+Green parts = where the DVI changes a lot
+Brown parts = negatively affected. By random events. 
 Yellow parts = ?
 
-See the affects of changing the grain
+See the effects of changing the grain
 Grain - the dimensions of the pixels. 
-By changing the grain (dimension of pixle), you will change the capability to cover a certain property
+By changing the grain (dimension of pixel), you will change the capability to cover a certain property
 Higher dimensions - hard to discover single plants. You will only see the forest
 Small dimensions - you can see a single tree/plant within the forest
 
 Lets change the grain of the images
-When changing the dimensions of the pixels it is called resempling: res
+When changing the dimensions of the pixels it is called resampling: res
 The function is: aggregate()
 Aggregate our image: aggregate(p224r63_2011)
 We need to express how much we want to aggregate it. How much we want to increase the factor
