f45c452fb3551250f811afdcea45bf953ad72311
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-05-01 11:34:39 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -1,5 +1,6 @@
 install.packages("sp")
 
+Recall the package
 library(sp)
 data(meuse)
 
@@ -8,24 +9,25 @@ meuse
 
 # Let us look at the first row of the set
 head(meuse)
-= means only to see some lines of the dataset. the first 6 rows will be visable
+= means only to see some lines of the dataset. the first 6 rows will be visible
 
 # Let us plot two variables 
 # Let us see if the zinc concentration is related to that of copper
 attach(meuse)
 = now the dataset it attached. and we can now start with the plotting.
-can also google: R software plot function (to see all differnt plot functions available) (x-axis,y-axis)
+can also google: R software plot function (to see all different plot functions available) (x-axis,y-axis)
 
 plot(zinc,copper)
 = graph visable in R
 
-change the graph (x-axis,y-axis,seperation) seperation: e.g colors. there is a need for " because colors are numbers
+change the graph (x-axis,y-axis,separation) separation: e.g colors. there is a need for " because colors are numbers
 plot(zinc,copper,col="blue") 
 
-To change the symbols in the graph- pointcharacter: pch
-google to see differnt pointcharacters and their numbers
+To change the symbols in the graph - point character: pch
+Google to see different point characters and their numbers
+Here we make us of the point character equals to 19
 plot(zinc,copper,col="blue",pch=19) 
 
-to make larger pointcharacters in the graph. character exaggeration: cex >1
-to make a smaller pointcharacters pick a numer in between 0-1 
+to make larger point characters in the graph. character exaggeration: cex >1
+to make a smaller point characters pick a number in between 0-1 
 plot(zinc,copper,col="blue",pch=19,cex=2)
