a59759a5bb726461d4864d7c5a8e33625ca5381c
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-06-03 17:58:59 +0200

    Create R_code_NO2.r

---
+++ R_code_NO2.r
@@ -0,0 +1,15 @@
+Download the data from iol
+The data is called NO2 data
+
+Open the downloaded file
+It contains 13 different images
+The images are ranging from January - March
+Showing the NO2 concentration
+
+Create a new folder NO2 into the lab folder
+Extract + import all the different images from the downloaded data into the NO2 folder
+
+Let set my working directory
+Which should be different
+Since we want to make use of data from our new folder
+setwd("C:/lab/NO2/")
