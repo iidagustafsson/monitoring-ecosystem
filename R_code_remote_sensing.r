60ae4b513a5eb4618d44823bdb093af46709abf7
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-06-20 14:58:35 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -1,30 +1,35 @@
-R code for remote sensing data analysis
+###############R code for remote sensing data analysis##################
 
+First, we need to install two new packages
 install.packages("raster")
 install.packages("RStoolbox")
 or use: install.packages(c("rasteer","RStoolbox"))
+Go to iol and download the file
+
+We will continue next lecture
 ----------------------------------------------------
-
 2020-02-22
-Unzip the downloaded file into the Lab folder. Make sure they are unpacked.
+Unzip the downloaded file into the Lab folder
+Make sure it is unpacked
 
-Set my working directory
+Set my working directory to lab folder
 setwd("C:/lab/")
 
 We are going to make use of the dataset raster
 Raster is the format of pixels
 Pixels = the quadrates forming the images
 
-Recall the dataset raster
+Recall the package raster
 library(raster)
 
 Go to the Lab folder. And find the data we downloaded (last time) it is called (p224r63...)
-path: 224 row: 63
-The files are from 2011 and 1988. In these years the images have been taken
+The name of the file corresponds to their location on the world map
+The numbers equals to coordinates - path: 224 row: 63
+The files are from 2011 and 1988. In these two years the images have been taken
 
 Import the images from the lab folder
-The function  to import images into R is called : brick
-Importing all of the packs of bands
+The function to import images into R is called: brick
+We use brick since we want to importing all of the packs of bands
 
 Go to the lab folder - .grd extension are the images (the extension cannot be seen in windows)
 grd = grid (easy way to store information)
@@ -41,31 +46,35 @@ plot(p224r63_2011)
 Vegetation is reflected by the Near Infrared (NIR) 
 High reflection in Green light, very little reflection in RED and BLUE due to photosynthesis
 
-Change the color ramp pallets  of the plots
+Change the color ramp pallets of the plots
 The color ramp is now from white to green colors
 We are going to change it into a grey scale ramp
 
-Assign the color ramp pallet to the name: cl
+Assign the color ramp palette to the name: cl
+To get a smooth transition between the colors: (100)
 cl<-colorRampPalette(c("black","grey","light grey"))(100)
+
+Plot the image and use the color ramp palette
 plot(p224r63_2011,col=cl)
 
 Bands of landset
 First band B1: Blue
 Second band B2: Green
 Third band B3: Red
-Fourth  band B4: NIR
+Fourth band B4: NIR
 Fifth band B5: Medium Infra Red
 Sixth band B6: Thermal Infra Red
-Seventh  band B7: Medium Infra Red
+Seventh band B7: Medium Infra Red
 
-Lets make a different plot of the image
+Let us make a different plot of the image
 Plot the single band. Just one of them
 Each of them can be colored with a single color ramp palette
 
+To plot several images at the same time
 Make us of the function: par
 Make a multi frame row: mfrow
 Explain the number of rows and colums you want: 2,2
-To put the number of colums and graphs into the graph. Make a series of character (c)
+To put the number of columns and graphs into the graph. Make a series of character (c)
 par(mfrow=c(2,2))
 
 To see all the colors available in R
@@ -79,14 +88,17 @@ The name of the band B1 = B1_sre
 plot(p224r63_2011$B1_sre,col=clb)
 
 ### Exercise - do the same for green band. Which is band B2
+Assign the color ramp pallet to the name: clg
 clg<-colorRampPalette(c("dark green", "green","light green"))(100)
 plot(p224r63_2011$B2_sre,col=clg)
 
 ### Exercise - do the same for red band. Which is band B3
+Assign the color ramp pallet to the name: clr
 clr<-colorRampPalette(c("dark red", "red","pink"))(100)
 plot(p224r63_2011$B3_sre,col=clr)
 
 ### Exercise - do the same for the NIR band. Which is band B4
+Assign the color ramp pallet to the name: cln
 cln<-colorRampPalette(c("red", "orange","yellow"))(100)
 plot(p224r63_2011$B4_sre,col=cln)
 
@@ -107,12 +119,12 @@ plot(p224r63_2011$B4_sre,col=cln)
 To close the window
 dev.off()
 
-Show a plot - RGB
-R-red =3, G-green =2, B-blue=1
-The function is called: plotRGB
-
-Stretch to see the colors, by making a linear stretching. 
-The function is called: stretch="Lin"
+Show a plot in red green blue plot (RGB).
+Plot an image of multi-layers
+The function is called: plotRGB()
+State which band should correspond to which layer: R-red =3, G-green =2, B-blue=1
+State to R which data to plot: p224r63_2011
+Stretch to see the colors, by making a linear stretching: stretch=”Lin”
 plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin")
 
 Change the bands scheme
@@ -157,7 +169,7 @@ ls()
 
 Which are: "cl" "clb" "clg" "cln" "clr" "landuse" "meuse" "p224r63_2011" "soiltype"  
 
-Recall the dataset raster. Raster means that it contains of pixels
+Recall the dataset raster. Raster means that the data contains of pixels
 library(raster)
 
 Import the other image. The one from 1988
@@ -166,7 +178,7 @@ brick("p224r63_1988_masked.grd")
 Give a name to this file
 p224r63_1988<-brick("p224r63_1988_masked.grd")
 
-Lets plot it into R
+Let us plot it into R
 plot(p224r63_1988)
 
 Bands of landset (same as for 2011)
@@ -196,7 +208,7 @@ But also creating problems for animals and plants
 Creating new niches for alien species to adapt to. Will compete with the endemic species.
 
 Use another stretch
-Lets make us of a histogram stretch: "hist"
+Let us make us of a histogram stretch: stretch="hist"
 This function enhances the 'noise'. 
 In this case it is mainly related to humidity. The evapotranspiration process of the forest will be visible 
 Put the both images in the same plot, but underneath one and another. Still using RGB 4 3 2 in both images
@@ -211,7 +223,7 @@ If there is a high amount of humidity - means the forest was more alive in 1988 
 What we want to do is comparing the vegetation index. Between the two images
 NIR is highly reflective. RED is very absorbed by chloroplasts 
 A healthy plant, has a very high reflection in NIR light while low reflection in RED light
-The difference between these index will give us the Vegetation Index.
+The difference between these indexes will give us the Vegetation Index.
 DVI (Difference Vegetation Index) = NIR-RED
 A dying plant, has less reflection in NIR light while higher reflection of RED light
 
@@ -224,11 +236,13 @@ Fifth band B5: Medium Infra Red
 Sixth band B6: Thermal Infra Red
 Seventh band B7: Medium Infra Red
 
-Lets give it the name to the index : dvi2011
+Let us start with the image from 2011
+Assign it the name to the index: dvi2011
 To link the band to the image, make use of $.  
 dvi2011<-p224r63_2011$B4_sre-p224r63_2011$B3_sre
+Now we are subtracting RED value with the NIR value
 
-Lets use a different color ramp palette
+Let us use a different color ramp palette
 cl<-colorRampPalette(c("darkorchid3", "light blue","lightpink4"))(100)
 plot(dvi2011,col=cl)
 
@@ -236,13 +250,13 @@ Try another color ramp palette
 cl<-colorRampPalette(c("darkorchid3","light blue","lightpink4"))(100) 
 plot(dvi2011,col=cl)
 
-### Exercise: dvi for 1988
+### Exercise- Make a DVI (Difference Vegetation Index) for 1988
 dvi1988<-p224r63_1988$B4_sre-p224r63_2011$B3_sre
 cl<-colorRampPalette(c("darkorchid3", "light blue","lightpink4"))(100)
 plot(dvi1988,col=cl)
 
 To make the differences from one year to another
-Make it the name : diff
+Give it the name : diff
 Assign the differences to this name: <-
 The differences: dvi2011 - dvi1988
 diff<-dvi2011 - dvi1988
@@ -253,24 +267,29 @@ Brown parts = negatively affected. By random events.
 Yellow parts = ?
 
 See the effects of changing the grain
-Grain - the dimensions of the pixels. 
+Grain - the dimensions of the pixels
 By changing the grain (dimension of pixel), you will change the capability to cover a certain property
 Higher dimensions - hard to discover single plants. You will only see the forest
 Small dimensions - you can see a single tree/plant within the forest
 
-Lets change the grain of the images
+Let us change the grain of the images
 When changing the dimensions of the pixels it is called resampling: res
 The function is: aggregate()
 Aggregate our image: aggregate(p224r63_2011)
-We need to express how much we want to aggregate it. How much we want to increase the factor
+We need to express how much we want to aggregate it. How much we want to increase the factor: fact=
 
 We are going to rename it to: p224r63_2011res
+And we want to aggregate it with the factor 10: fact=10
 p224r63_2011res10<-aggregate(p224r63_2011,fact=10)
+
+Now we want to aggregate it with the factor 100: fact=100
 p224r63_2011res100<-aggregate(p224r63_2011,fact=100)
 
-Now we want to make the par of the different dimensions. 
-The original, the dimensions with factor 10 and with the dimension of factor 100
-Putting them in three rows and in one column
+Now we want to see all the images at the same time
+Make use of the par multi frame row function: par(mfrow 
+State how many rows and columns: 3,1
+We want to plot it with the NIR in the red band, the red in the green band and the green in the blue band
+We want to plot the original, the dimensions with factor 10 and with the dimension of factor 100
 par(mfrow=c(3,1))
 plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
 plotRGB(p224r63_2011res10,r=4,g=3,b=2,stretch="Lin")
