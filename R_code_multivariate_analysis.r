8c0d0dcf20822ae61369b92483632fb1795ea291
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-06-20 13:22:20 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -1,12 +1,12 @@
 ############ Multivariate Analysis Notes#################
 -------------------2020-04-08-------------------
-How the different communites are changing in time
-And how we can monitore the changes
+How the different communities are changing in time
+And how we can monitor the changes
 
 Start with in-situ data collection
 Going out in the field to get information about the species that live in the field
 
-We are dealing with communities. Also soil, temperature, pH etc
+We are dealing with communities. Also, soil, temperature, pH etc
 
 Butterflies first plot - 13 , second plot - 1 and third plot - 2
 Orchys: first plot - 15, second plot - 2 and third plot - 3
@@ -15,7 +15,7 @@ Larix: first plot - 3, second plot - 10 and third plot - 8
 See how the 3 species are related to each other
 One manner to see this data. 
 Do a matrix/table of each plot. And see where the species have been find + how many individuals
-As we can see the the butterflies and orchys are more related, rather than butterflies and tree
+As we can see the butterflies and orchys are more related, rather than butterflies and tree
 
 Take number of the individuals as the value of the 
 x-axis = plot 1 
@@ -25,16 +25,16 @@ z-axis = plot 3
 Species that are more related are closer to each other
 Species that are less related are farer away
 
-The functional relationship, in our case, is the relationhip between butterflies and orchys
+The functional relationship, in our case, is the relationship between butterflies and orchys
 Study ecosystem functions and relationships. Which have been studied in a certain area
 
-What if we use a 4 plots?
+What if we use 4 plots?
 How to show 4 plots in such a graph?
 It is impossible. We can only see 3 dimensions. 
-The forth one is time
+The fourth one is time
 
 How to reduce a 4th dimension model into a 3th dimension object?
-Several plots. That should be reduced into 2 dimension
+Several plots. That should be reduced into 2 dimensions
 Multi=Several
 Varib=variables
 
@@ -45,26 +45,17 @@ Plot 1 explained 50% of variation. Same for plot 2
 Component 1 explain 90% of the variation, and component 2 explain 10%
 
 2020-04-15
-Takin plots in space some where, and each plot have different species. 
-The species are differnt distribute in the plots. Count the speceis
-Use the number of individuals as corrdinates
+Takin plots in space somewhere, and each plot have different species. 
+The species are different distribute in the plots. Count the species
+Use the number of individuals as coordinates
 See correlations between species
 Multivariate analysis -taking several plots - redesign them to see them into 2D
 
 Intro - Description of the Dataset
 Based on thinking of different plots
 Matrix of 
-Tropicl Forest - Biomes. High amopunt of evaporation, rain, light
+Tropical Forest - Biomes. High amount of evaporation, rain, light
 All plants can live together. High biodiversity. 
 Broadleaf continental forest - Biome
 Coniferous forest
-Artic Thundra
-
-
-
-
-
-
-
-
-
+Artic Tundra
