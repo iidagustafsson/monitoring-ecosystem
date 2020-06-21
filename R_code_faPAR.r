df0cd5e53d2b72fe34dae512faf7eaff335ba548
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-06-21 17:03:58 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -1,4 +1,4 @@
-Chemical cycling from Satallite images
+Chemical cycling from Satellite images
 
 We are going to make use of raster, rasterVis and rasterdiv
 Recall the libraries
@@ -6,29 +6,30 @@ library(raster)
 library(rasterVis)
 library(rasterdiv)
 
-Take a look at the latest data whe used last time
-Dealing with workwide 
+Take a look at the latest data. The data we used last time
+Dealing with worldwide
 
-copNDVI - copernicus reflectense in the NIR, and small reflection in Red (due to photosysnthesis)
-Then do the difference between these values
+copNDVI - Copernicus reflectance in the NIR, and small reflection in Red (due to photosynthesis)
+We will measure the difference between these values
 
-Lets plot the NDVI
+Let us plot the NDVI
 plot(copNDVI)
 
-blue color is a code - it represent the water
-reclassigying 
+Blue color represent the water
+reclassifying 
 copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
 
-level plot of this NDVI
-restart from here
+Make a level plot of this NDVI: levelplot()
+To see the NDVI values with respect to the world map
+State which data to level plot: copNDVI
 levelplot(copNDVI)
 
-The value of NDVI is larger around the equator - main primary forest = increasing the biomass
-The value of NDVI is lower around the equator 
-Another peak in the NDVi. At the top of the graph. This is due to the broad leaf and cornifor forest
-The equator forest is vertically structured. While the cor
-The taiga is not structured in 3D. The individuals are very similar to each other
-The vertical structure os very poor. The individuals are very similar
+The value of NDVI is larger within the equator - mainly primary forest = increasing the biomass
+The value of NDVI is lower 30 degrees around the equator – mainly because of deserts
+Another peak in the NDVI. At the top of the graph. This is due to the broad leaf and conifer forest
+The equator forest is longitudinal structured. While the conifer forest is latitudinal structured
+The taiga is not structured in 3 dimensions. The individuals/species are very similar to each other 
+The vertical structure is very poor. The individuals are very similar
 No plants on top of each other. Usually just one species at one place
 
 Set my working directory
@@ -36,35 +37,33 @@ setwd("C:/lab/")
 
 Now we can enter the faPAR
 The faPAR is called faPAR10
-Go to the lab fodler
-Copy the name f2
+Go to the lab folder
 
-Let us inport the data
+Let us import the data
 Make us of the function: raster
 Give it the name: faPAR10
-10 meaning we used the factor 10 aggregated the data
+10 meaning we used the factor 10 aggregated the data (professor already did for us)
 faPAR10 <- raster("faPAR10.tif")
 
-Make the same level plot with the faPAR10 instead of the 
-PAR in cap - because R is sensative
+Make the same level plot we did, but with the faPAR10 instead of the 
+Important to have PAR in cap - because R is sensitive. To not risk any misunderstandings
 levelplot(faPAR10)
 
-High NDVI in the north hemisphere and aroudn the equator
-What is happening to the faPAR
-The equator has a very high amount of photosyntheis in this area
-Not all sunlight that is coming, is not going to the soik
+High NDVI in the north hemisphere and around the equator
+What is happening to the faPAR?
+The equator has a very high amount of photosynthesis in this area
+Not all sunlight that is incoming, is reaching the soil
 Most light is used by the plants
 In top of the norther part. The faPAR is not as high as the NDVI
-We are not only considering of the energy
+We are not only considering the energy
 We are considering the chemical cycling by plants
-In the conifer forest most light is going into the soil. the light is not used
-The main point: one individual after one an another. next to each other. very similar. and much light not used
-Tropical forest: the opposite - many different individuals, complext structure
-
+In the conifer forest most light is going into the soil. All incoming light is not used by plants
+Conifer forest: one individual after one an another. next to each other. very similar. and much light not used by plant = can easily reach the soil
+Tropical forest: the opposite - many different individuals next to one and another, complex structures and almost no sunlight reaches the soil
 
 Save as pdf, both NDVI image and faPAR10
 Use the pdf function: pdf("")
-Use the " - since we are going to save it ouside of R
+Use the " - since we are going to save it outside of R
 Give it the name copNDVI.pdf and faPAR10.pdf
 
 pdf("copNDVI.pdf")
@@ -74,17 +73,3 @@ dev.off()
 pdf("faPAR.pdf")
 levelplot(faPAR10)
 dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
