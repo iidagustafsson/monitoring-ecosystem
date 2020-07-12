f47856bf78277530f4e92cbb8ad6070e433efc69
iidagustafsson <62661806+iidagustafsson@users.noreply.github.com>
2020-07-12 14:39:19 +0200

    Update R_code_pca_remote_sensing.r

--- R_code_pca_remote_sensing.r
+++ R_code_pca_remote_sensing.r
@@ -19,10 +19,6 @@ Use “ “ since we are going outside R
 Assign the function to the new name: p224r63_2011
 p224r63_2011<-brick("p224r63_2011_masked.grd")
 
-graphical file 
-header- explaining the names (.hdr)
-linking the graphical 
-
 Make use of 3 different landsat band sets
 First Band B1: Blue
 Second Band B2: Green
@@ -31,10 +27,10 @@ Fourth Band B4: NIR
 Fifth Band B5: (Short-wave) Medium Infra-Red (different range of wavelength within spectrum)
 Sixth Band B6: Thermal infrared
 Seventh Band B7: (Short-wave) Medium Infra-Red (different range of wavelength within spectrum)
-Eight Band B8 Panchromatic
+Eight Band B8: Panchromatic
 
 We can plot in different ways: plotRGB, GGplot
-Stretch to see the colors, by making a linear stretching. 
+Stretch to see the colors, by making a linear stretching 
 The function is called: stretch="Lin"
 We want to describe the different bands (Medium infra-red, NIR, RED) by using the primary colors: RED, GREEN, BLUE
 plotRGB(p224r63_2011,r=5,g=4,b=3,stretch="Lin")
@@ -48,15 +44,15 @@ State which bands to use - Medium infra-red, NIR, RED: 5,4,3
 ggRGB(p224r63_2011,5,4,3)
 
 ### Exercise - Produce the same procedure but with the 1988 image
-Upload the image into R
-Give it the name
-Plot it into the software
+1. Upload the image into R
+2. Give it the name
+3. Plot it into the software
 
 p224r63_1988<-brick("p224r63_1988_masked.grd")
 plotRGB(p224r63_1988,r=5,g=4,b=3,stretch="Lin")
 ggRGB(p224r63_1988,5,4,3)
 
-To see both the images at the same time. 
+To see both the images at the same time 
 Make use of the function: par
 par(mfrow=c(1,2))
 plotRGB(p224r63_1988,r=5,g=4,b=3,stretch="Lin")
@@ -69,7 +65,7 @@ How to see all the bands together? All the information together in one plot
 Let us see if the bands are correlated to each other or not
 First of all, what does it mean correlated?
 Being correlated - means you are following the pattern of another variable
-Example the number of trips and the number of infected by covid are related 
+Example the number of trips/travels and the number of infected by covid are related 
 
 Let us see if B1 and B3 are corelated to each other
 Blue and Red should have a high correlation. Because this light is used to fixate photosynthesis
@@ -83,7 +79,6 @@ names(p224r63_2011)
 Here you have the names of the different bands: B1_sre and B3_sre
 We are interested in plotting the band 1 and band 3
 
-
 Make us of the image from 2011
 Linking different parts
 By using the $ symbol
@@ -93,7 +88,7 @@ dev.off()
 
 Now linking the image to B1 and B3
 plot(p224r63_2011$B1_sre, p224r63_2011$B3_sre)
-High correlation. 
+B1 and B3 have high correlation 
 
 Positive correlation between variables. Both variables are increasing with respect to the other
 Negative correlation between variables. One variable is decreasing with respect to the other
@@ -103,7 +98,7 @@ We can apply
 Principal component = PC1 - accounting for 90% 
 Principal component = PC2 - accounting for 10%
 
-We need to decrease the resolution of the image.
+We need to decrease the resolution of the image
 Why? The PCA is a hard. We have a huge amount of data
 Decrease it by a factor of 10. Passing from 4 M of data -> into 40K of data
 Decreasing on the linear dimension
@@ -133,7 +128,7 @@ plot(p224r63_2011_pca$map, col=cl)
 
 The PC1 - is accounting for almost all the information of the variations (99,83%)
 
-Let us see how the variations of each component in the model. 
+Let us see how the variations of each component in the model 
 We are interest in the information of the model. We want to see the output of the model
 Make use of the function: summary
 summary(p224r63_2011_pca$model)
@@ -146,7 +141,7 @@ Band 4 (NIR) has a lower correlation with respect to the other bands
 Let us plot the first 3 components
 Make use of 3 different colors (Blue, Green and Red)
 The first component accounting for 99,83%
-The second component accounting for 0,146 %
+The second component accounting for 0,146%
 The third component accounting for 0,0157%
 
 We are going to plot each principle components into the bands
@@ -168,21 +163,22 @@ summary(p224r63_1988_pca$model)
 
 Very high correlation between the bands
 99,56% by PC1 (the image from 2011 has 99,83%)
-0,38% by PC2 and 0,048 by PC3
+0,38% by PC2 (the image from 2011 has 0,146%)
+0,048% by PC3 (the image from 2011 has 0,0157%)
 
 Make us of the function pairs to make a scatterplot by the variables in 1988
 pairs(p224r63_1988)
-Very high correlation between the bands. 
+Very high correlation between the bands
 
 Now make a difference between these two pca maps
 Plot the difference between the two images
 Let us take the map from 2011 subtracted by the map from 1988
-For each pixel we have two different maps. 
+For each pixel we have two different maps 
 Assign it the name: <-
 Give it the name: difpca  (different pca)
 difpca<-p224r63_2011_pca$map-p224r63_1988_pca$map
 
-plot these differences among the single components. 
+plot these differences among the single components 
 plot(difpca)
 
 We will change the color ramp palette
@@ -190,7 +186,7 @@ Assign it the name: <-
 Give it the name: cldif
 cldif<-colorRampPalette(c('blue','black','yellow'))(100)
 
-Now we are going to only plot the Principle Component 1 PC1
+Now we are going to only plot the Principle Component 1: PC1
 To see the name, just run the name
 difpca
 
